45c5bc7ab137a2b2b0118c1149815c85ebce6a2e
KLIKEARUSSIAN <rezzxy64@gmail.com>
2025-07-08 04:24:40 -0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -26,7 +26,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
       { body: commands },
     );
 
-    console.log('Successfully reloaded application (/) commands.');
+    console.log('✅ Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
   }
